3df217783d52a4509970c13fe3d984805f05909b
kangyahua <kangyahua@souche.com>
2019-09-03 13:08:09 +0800

    go delegation pattern

--- gomethod.go
+++ gomethod.go
@@ -15,6 +15,16 @@ func (p *Point) ScaleByPointer(factor float64) {
     p.Y *= factor
 }
 
+
+type ColoredPoint struct {
+    *Point
+    color string
+}
+
+func (p *ColoredPoint) ScaleBy(factor float64) {
+    p.Point.ScaleByPointer(factor)
+}
+
 func main() {
 	point := Point{1,2}
 	fmt.Println(point)
@@ -24,4 +34,11 @@ func main() {
 
 	point.ScaleByPointer(2)
 	fmt.Println(point)
+
+	p := ColoredPoint{&Point{1, 1}, "red"}
+	q := ColoredPoint{&Point{5, 4}, "blue"}
+	fmt.Println(*q.Point) // "5"
+	q.Point = p.Point                 // p and q now share the same Point
+	p.ScaleBy(2)
+	fmt.Println(*p.Point, *q.Point) // "{2 2} {2 2}"
 }
